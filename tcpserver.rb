4eddd72d69487e3f4a1d7855aafa0008fbbe0800
sawa777yu <sawa.oranger7@gmail.com>
2024-03-10 16:32:15 +0900

    first commit

---
+++ tcpserver.rb
@@ -0,0 +1,21 @@
+require 'socket'
+
+puts("=== サーバー起動 ===")
+
+begin
+  server_socket = TCPServer.new("localhost", 8080)
+  # 外部からの接続を待ち、接続があったらコネクションを確立する
+  puts("=== クライアントからの接続を待ちます ===")
+  client_socket, address = server_socket.accept()
+  puts("=== クライアントとの接続が完了しました remote_address: #{address} ===")
+  # クライアントから送られてきたデータを取得する
+  request = client_socket.recv(4096)
+
+  # クライアントから送られてきたデータをファイルに書き出す
+  file = File.open("server_recv.txt", "w")
+  file.write(request)
+  # 返事は特に返さず、通信を終了させる
+  client_socket.close()
+ensure
+  puts("=== サーバー停止 ===")
+end
